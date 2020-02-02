6fadf62e53c2754ad08dd50e8ada5c15e241d1e4
Patryk <szwaj.pat@gmail.com>
2020-02-02 13:01:05 +0100

    refactor engine, working json converter

--- src/main/java/engine/MainJson.java
+++ src/main/java/engine/MainJson.java
@@ -2,14 +2,26 @@ package engine;
 
 import employee.Employee;
 import readers.JsonTestReader;
-
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainJson {
     public static void main(String[] args) {
         JsonTestReader jsonreader = new JsonTestReader();
         List<Employee> employees = jsonreader.JsonTestReader();
 
+        //creating empty map
+        Map<String, Double> workingmap = new HashMap<>();
 
+        //initializing the map with zeros instead of nulls
+        employees.forEach(c -> workingmap.put(c.getJob(),0.0));
+
+        //parsing the salaries to doubles and adding them to the map using 'job' as a key
+        employees.forEach(c -> workingmap.put(c.getJob(),
+                Double.parseDouble(c.getSalary().replace(',','.'))+workingmap.get(c.getJob())));
+
+        //prining the sums
+        System.out.println(workingmap.entrySet());
     }
 }
