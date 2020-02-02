c90ffa4f3a26d837a116418d514a7c552a95bf3f
Patryk <szwaj.pat@gmail.com>
2020-02-02 13:16:40 +0100

    added csv reader

--- src/main/java/readers/ExcelReader.java
+++ src/main/java/readers/ExcelReader.java
@@ -1,4 +1,41 @@
 package readers;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ExcelReader {
+    public static void main(String[] args) {
+
+        String csvFile = "../net.voicebot.inittask/src/main/resources/employees(2).csv";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ";";
+
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+
+                String[] emp = line.split(cvsSplitBy);
+
+                System.out.println("Employee [job= " + emp[3] + " , salary=" + emp[4] + "]");
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
 }
