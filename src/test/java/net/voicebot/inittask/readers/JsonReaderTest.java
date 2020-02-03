a02d82eb17e0b2cdcb75a7f579c8421533113cac
Patryk <szwaj.pat@gmail.com>
2020-02-03 18:44:56 +0100

    refactor tests

--- src/test/java/net/voicebot/inittask/readers/JsonReaderTest.java
+++ src/test/java/net/voicebot/inittask/readers/JsonReaderTest.java
@@ -1,4 +1,22 @@
 package net.voicebot.inittask.readers;
 
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 public class JsonReaderTest {
+
+    JsonTestReader jsonReader = new JsonTestReader();
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Test()
+    public void fileNotFound(){
+        exception.expect(NullPointerException.class);
+        jsonReader.read("test.json");
+    }
+
+    @Test()
+    public void readerRunTest(){
+        jsonReader.read("employeesTest.json");
+    }
 }
+
